7b3fea23a3abea327604a16a5766f119c14cb878
umacke <umacke@LILPCF0003.poc.local>
2016-07-27 17:34:03 +0200

    ttt

---
+++ src/test/java/com/uma/project/jsonDataParserTest.java
@@ -0,0 +1,32 @@
+package com.uma.project;
+
+import com.google.common.io.CharStreams;
+import com.uma.project.services.data.parser.json.JsonDataParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.junit.Test;
+
+public class jsonDataParserTest {
+
+    @Test
+    public void parseTeamsTest() {
+
+        String jsonData = "";
+        try {
+            jsonData = this.loadFileString("/teams.json");
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("resultat2 = " + new JsonDataParser().parseTeams(jsonData).toString());
+    }
+
+    public String loadFileString(String file) throws IOException {
+        final InputStream xmlStream = this.getClass().getResourceAsStream(file);
+        final String teams = CharStreams.toString(new InputStreamReader(xmlStream));
+        return teams;
+    }
+}
